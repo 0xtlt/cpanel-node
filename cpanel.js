9f1d45279d9dfefe26d6b53c67ea8bd68118ab03
Thomas Tastet <31560900+ThomasT404@users.noreply.github.com>
2019-04-12 12:33:44 +0200

    🚀 code review and optimization

--- cpanel.js
+++ cpanel.js
@@ -1,18 +1,59 @@
 const request = require("request");
 
+/**
+ * @param {*} a 
+ * @param {*} b
+ * 
+ * @returns {boolean}
+ */
 const tif = function(a, b){
     return (typeof a === b);
 };
 
+/**
+ * @param {any} [testedValues]
+ * 
+ * @returns {boolean}
+ */
+const tifEvolved = function(testedValues){
+    let isOkay = true;
+
+    if(testedValues.length % 2 === 1)
+        return false;
+
+    for(let i = 0; i < testedValues.length; i+=2){
+        if(typeof testedValues[i] !== testedValues[i+1]){
+            isOkay = false;
+        }
+    }
+
+    return isOkay;
+}
+
 class CPanel {
+    /**
+     * @param {object} options
+     * @param {string} options.host
+     * @param {string} options.user
+     * @param {string} options.pass
+     * @param {string} options.boolean
+     * @param {string} options.port
+     */
     constructor(options){
-        if(options === undefined || !tif(options, "object") || !tif(options.host, "string") || !tif(options.user, "string") || !tif(options.pass, "string") || !tif(options.https, "boolean") || !tif(options.port, "string"))
+        if(!tifEvolved([options, "object", options.host, "string", options.user, "string", options.pass, "string", options.https, "boolean", options.port, "string"]))
             throw new Error("The params option<Object> must be completed with host<String>, user<String>, pass<String>, https<Boolean>, port<String>");
 
         this.options = options;
     }
 
-    login(module, func, values = []) {
+    /**
+     * @param {string} module_ 
+     * @param {string} func 
+     * @param {object} values
+     * 
+     * @returns {Promise}
+     */
+    login(module_, func, values = []) {
         const me = this;
         return new Promise(function(resolve, reject) {
             let value = '';
@@ -21,7 +62,7 @@ class CPanel {
             });
 
             let call = '/json-api/cpanel?cpanel_jsonapi_user='+me.options.user;
-                call += '&cpanel_jsonapi_apiversion=2&cpanel_jsonapi_module='+module;
+                call += '&cpanel_jsonapi_apiversion=2&cpanel_jsonapi_module='+module_;
                 call += '&cpanel_jsonapi_func='+func;
                 call += value;
 
@@ -38,10 +79,20 @@ class CPanel {
         });
     };
 
+
+    /**
+     * @param {object} options
+     * @param {string} option.domain
+     * @param {string} option.email
+     * @param {string} option.password
+     * @param {number} option.quota
+     * 
+     * @returns {Promise}
+     */
     emailAddpop(options){
         const me = this;
         return new Promise(function(resolve, reject) {
-            if(!tif(options, "object") || !tif(options.domain, "string") || !tif(options.email, "string") || !tif(options.password, "string") || !tif(options.quota, "number"))
+            if(!tifEvolved([options, "object", options.domain, "string", options.email, "string", options.password, "string", options.quota, "number"]))
                 return reject(new Error("The params options<Object> must be completed with domain<String>, email<String>, password<String>, quota<Number>"));
 
             me.login('Email', 'addpop', [{ domain: options.domain },{ email: options.email },{ password: options.password }, { quota: options.quota }]).then(function(obj) {
@@ -52,10 +103,17 @@ class CPanel {
         });
     };
 
+    /**
+     * @param {object} options
+     * @param {string} options.email
+     * @param {string} options.domain
+     * 
+     * @returns {Promise}
+     */
     emailDelpop(options){
         const me = this;
         return new Promise(function(resolve, reject) {
-            if(!tif(options, "object") || !tif(options.email, "string") || !tif(options.domain, "string"))
+            if(!tifEvolved([options, "object", options.email, "string", options.domain, "string"]))
                 throw new Error("The params options<Object> must be completed with email<String>, domain<String>");
 
             me.login('Email', 'delpop', [{ email: options.email }, { domain: options.domain }]).then(function(obj) {
@@ -66,10 +124,18 @@ class CPanel {
         });
     }
 
+    /**
+     * @param {object} options
+     * @param {string} options.email
+     * @param {string} options.domain
+     * @param {string} options.password
+     * 
+     * @returns {Promise}
+     */
     emailPasswdpop(options){
         const me = this;
         return new Promise(function(resolve, reject){
-            if(!tif(options, "object") || !tif(options.email, "string") || !tif(options.domain, "string") || !tif(options.password, "string"))
+            if(!tifEvolved([options, "object", options.email, "string", options.domain, "string", options.password, "string"]))
                 throw new Error("The params options<Object> must be completed with email<String>, domain<String>, password<String>");
 
             me.login('Email', 'passwdpop', [{email: options.email},{password: options.password}, {domain: options.domain}]).then(function (obj) {
@@ -80,10 +146,18 @@ class CPanel {
         });
     }
 
+    /**
+     * @param {object} options
+     * @param {string} options.email
+     * @param {string} options.domain
+     * @param {number} options.quota
+     * 
+     * @returns {Promise}
+     */
     emailEditpopQuota(options){
         const me = this;
         return new Promise(function(resolve, reject){
-            if(!tif(options, "object") || !tif(options.email, "string") || !tif(options.domain, "string") || !tif(options.quota, "number"))
+            if(!tifEvolved([options, "object", options.email, "string", options.domain, "string", options.quota, "number"]))
                 throw new Error("The params options<Object> must be completed with email<String>, domain<String>, quota<Number>");
 
             me.login('Email', 'editquota', [{email: options.email},{quota: options.quota}, {domain: options.domain}]).then(function (obj) {
@@ -94,6 +168,9 @@ class CPanel {
         });
     }
     
+    /**
+     * @returns {Promise}
+     */
     zoneFetchZones(){
         const me = this;
         return new Promise(function(resolve, reject){
@@ -105,10 +182,18 @@ class CPanel {
         });
     }
 
+    /**
+     * @param {object} options
+     * @param {string} options.domain
+     * @param {string} options.name
+     * @param {string} options.type
+     * 
+     * @returns {Promise}
+     */
     zoneFetchZone(options){
         const me = this;
         return new Promise(function(resolve, reject){
-            if(!tif(options, "object") || !tif(options.domain, "string") || !tif(options.name, "string") || !tif(options.type, "string"))
+            if(!tifEvolved([options, "object", options.domain, "string", options.name, "string", options.type, "string"]))
                 throw new Error("The params options<Object> must be completed with domain<String>, name<String>, type<String>");
 
             me.login('ZoneEdit', 'fetchzone',[{domain: options.domain},{name: options.name},{type: options.type}]).then(function (obj) {
@@ -121,18 +206,15 @@ class CPanel {
 
     /**
      * Addons domains
+     * @param {regex} regex
+     * 
+     * @returns {Promise}
      */
-    listAddonDomains(options){
+    listAddonDomains(regex = ""){
         const me = this;
         return new Promise(function(resolve, reject){
-            if(!tif(options, "object") || !tif(options.regex, "string")){
-                options = {
-                    regex: ''
-                };
-            }
-
             // List the account's addon domains.
-            me.login('AddonDomain', 'listaddondomains', [{ regex: options.regex }]).then(function (obj) {
+            me.login('AddonDomain', 'listaddondomains', [{ regex }]).then(function (obj) {
                 return resolve(obj);
             }, function (err) {
                 return reject(err);
@@ -142,11 +224,19 @@ class CPanel {
 
     /**
      * Add domain
+     * @param {object} options
+     * @param {string} options.domain
+     * @param {string} options.name
+     * @param {string} options.type
+     * @param {string} options.address
+     * @param {number} options.ttl
+     * 
+     * @returns {Promise}
      */
     zoneAddDomain(options) {
         const me = this;
         return new Promise(function (resolve, reject) {
-            if (!tif(options, "object") || !tif(options.domain, "string") || !tif(options.name, "string") || !tif(options.type, "string") || !tif(options.address, "string"))
+            if (!tifEvolved([options, "object", options.domain, "string", options.name, "string", options.type, "string", options.address, "string"]))
                 throw new Error("The params options<Object> must be completed with domain<String>, name<string>, type<string> and address<string>");
 
             let values = [{ domain: options.domain }, { name: options.name }, { type: options.type }, { address: options.address }];
@@ -164,11 +254,20 @@ class CPanel {
     
     /** 
      * Edit domain
+     * @param {object} options
+     * @param {string} options.domain
+     * @param {number} options.line
+     * @param {string} options.address
+     * @param {string} options.type
+     * @param {string} options.name
+     * @param {number} options.ttl
+     * 
+     * @returns {Promise}
     */
     zoneEditDomain(options) {
     const me = this;
     return new Promise(function (resolve, reject) {
-        if (!tif(options, "object") || !tif(options.domain, "string") || !tif(options.line, "number"))
+        if (!tifEvolved([options, "object", options.domain, "string", options.line, "number"]))
             throw new Error("The params options<Object> must be completed with domain<String> and Line<number>");
 
         let values = [{ domain: options.domain }, { line: options.line }];
@@ -196,11 +295,16 @@ class CPanel {
 
     /** 
      * Remove domain
+     * @param {object} options
+     * @param {string} options.domain
+     * @param {number} options.line
+     * 
+     * @returns {Promise}
     */
     zoneRemoveDomain(options) {
         const me = this;
         return new Promise(function (resolve, reject) {
-            if (!tif(options, "object") || !tif(options.domain, "string") || !tif(options.line, "number"))
+            if (!tifEvolved([options, "object", options.domain, "string", options.line, "number"]))
                 throw new Error("The params options<Object> must be completed with domain<String> and Line<number>");
 
             let values = [{ domain: options.domain }, { line: options.line }];
